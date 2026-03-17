f355ba646cc2d6dfb960d015cbe39cfc9d693c1f
mcollis <collis.michael@gmail.com>
2026-03-17 07:11:31 -0700

    Initial dotfiles: zsh, tmux, and neovim configs
    Managed with GNU Stow. Includes LazyVim setup with
    catppuccin theme and tmux-navigator integration.

---
+++ .config/nvim/lua/plugins/theme.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    opts = {
+      flavour = "mocha",
+      transparent_background = false,
+      term_colors = true,
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+}
